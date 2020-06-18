ba16703fcb5ad5839009bcc82802159d54b82bda
SleepyJen <jennifer.ni0924@gmail.com>
2020-06-17 23:13:22 -0700

    added styling to index html

--- controller/votesAPI.js
+++ controller/votesAPI.js
@@ -2,8 +2,36 @@ const express = require('express');
 const router = express.Router();
 const db = require('../models');
 
-router.get('/get', (req, res) => {
+router.get('/', (req, res) => {
+    db.Votes.find({}).then(result => {
+        res.json(result);
+    });
+});
 
+router.get('/getName', (req, res) => {
+    db.Votes.find({ name: req.body.name }).then(result => {
+        res.json(result);
+    });
+});
+
+router.post('/name', (req, res) => {
+    db.Votes.create({
+        name: req.body.name
+    }).then(result => {
+        res.json(result);
+    });
+});
+
+router.post('/choices', (req, res) => {
+    db.Votes.findOneAndUpdate({ name: req.body.name },
+        {
+            $push: {
+                votes: req.body.votes
+            }
+        }
+    ).then(result => {
+        res.json(result);
+    });
 });
 
 module.exports = router;
